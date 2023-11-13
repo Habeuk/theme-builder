62c7d183e7c05de6b77bb6f1f7f14efc41baae7f
Dassi Dieudonne <dassidieu@gmail.com>
2023-11-13 14:45:15 +0100

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ const plugins = [];
 /**
  * Selectionner le theme à afficher lors de la commande npm run serve.
  */
-const CurrentThemeName = "price";
+const CurrentThemeName = "affiche";
 
 plugins.push(
   new MiniCssExtractPlugin({
