2e87abc6dce9af61807e0096e1ef6042e465b176
vystii <ngahabanda@gmail.com>
2023-11-16 13:59:40 +0100

    première version section information sur le produit et initialisation de la deuxième gallery

--- src/nebuleuse/gallery-swiper/gallery-swiper.js
+++ src/nebuleuse/gallery-swiper/gallery-swiper.js
@@ -1,15 +1,50 @@
 import { getScript } from 'jquery';
 import './gallery-swiper.html'
 import './gallery-swiper.scss'
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Autoplay, Controller } from 'swiper';
 
+// autoplay: {
+//     delay: 5000,
+//     disableOnInteraction: false
+// },
 const swiper = new Swiper(".swiper", {
-    modules: [Navigation, Pagination],
-    speed: 400
+    modules: [Autoplay, Controller],
+    speed: 500,
 })
-swiper.on('slideChange', function () {
-    console.log('slide changed');
-});
+
+const swiper2 = new Swiper(".slider-thumb", {
+    modules: [Autoplay, Controller],
+    speed: 500,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    touchRatio: 0.2,
+    slideToClickedSlide: true,
+})
+swiper.controller.control = swiper2
+swiper2.controller.control = swiper
+// swiper.on('slideChange', function () {
+//     console.log('slide changed');
+// });
+
 // getScript('https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', () => {
-//     const swiper = new Swiper('.swiper')
+//     const swiper = new Swiper('.swiper', {
+//         modules: [Pagination, Navigation, Autoplay, Zoom],
+//         slidesPerView: 1,
+//         speed: 500,
+//         loop: true,
+//         pagination: {
+//             el: '.swiper-pagination',
+//             clickable: true,
+//             dynamicBullets: true
+//         },
+//         navigation: {
+//             nextEl: '.swiper-button-next',
+//             prevEl: '.swiper-button-prev',
+//         },
+//         autoplay: {
+//             delay: 5000,
+//             disableOnInteraction: false
+//         },
+//         zoom: true,
+//     })
 // })
