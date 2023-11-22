b61ba8d5ac9803147e557c9ac3f8b1fa4b16d339
Justin-Jordan <justi2017@outlook.fr>
2023-11-22 17:36:57 +0100

    add some sections

--- src/commerce/commerce.js
+++ src/commerce/commerce.js
@@ -3,3 +3,5 @@ import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 import "./commerce.scss";
 import "./advantage/advantage";
 import "./promoted-product/promoted-product";
+import "./newsletter/newsletter"
+import "./vertical-banner/vertical-banner"
