c2ca67c9c79539696340613479521bd283f5d615
vystii <ngahabanda@gmail.com>
2023-11-27 17:02:04 +0100

    ajout de la section slider pour le theme drupal cforge

--- src/cforge/cforge.js
+++ src/cforge/cforge.js
@@ -1,5 +1,6 @@
 //sections import
+// import "./cforge.scss";
 import "@stephane888/wbu-atomique/js/bootstrap/all.js";
-
 // import des sections :
 import "./cforge-menu/cforge-menu";
+import "./cforge-slider/cforge-slider";
