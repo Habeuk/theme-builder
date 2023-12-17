02e7704c32a69b1c9c23fed8b9f7ed96fe141ef4
kouwa stephane <kksasteph888@gmail.com>
2023-12-17 08:23:01 +0100

    Maj

--- src/nebuleuse/nebuleuse.js
+++ src/nebuleuse/nebuleuse.js
@@ -7,7 +7,7 @@ import "./nebuleuse.scss";
 
 // === importation des sections ===
 
-// import "./gallery-swiper/gallery-swiper"
-// import "./commerce-gallery/commerce-gallery"
-import "./commerce-gallery-v3/commerce-gallery";
-//import "./commerce-gallery-v2/commerce-gallery";
+// import "./gallery-swiper/gallery-swiper";
+import "./commerce-gallery/commerce-gallery";
+// import "./commerce-gallery-v3/commerce-gallery";
+// import "./commerce-gallery-v2/commerce-gallery";
