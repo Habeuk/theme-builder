bdca024208f928d8e2e6e17759ab21e305064cde
laurelle <obilamessomoangelelaurelle@gmail.com>
2024-01-15 09:54:23 +0000

    create footer

---
+++ src/hotel/hotel.js
@@ -0,0 +1,5 @@
+import "./hotel.scss";
+// import "./evenex-countdown/countdown"
+//import "@stephane888/wbu-atomique/js/bootstrap/all.js";
+
+import "./type_section";
---
+++ src/hotel/type_section/type_section.js
@@ -0,0 +1,2 @@
+import "./type_section.html";
+import "./type_section.scss";
