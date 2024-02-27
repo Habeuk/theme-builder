44d492b8cf56f303c93de126f14075f02ed4bc15
kouwa stephane <kksasteph888@gmail.com>
2024-02-27 16:38:07 +0100

    Maj

--- src/spa/testslider/index.js
+++ src/spa/testslider/index.js
@@ -4,6 +4,46 @@ import "./index.html";
  * Pour l'env. Drupal, il faudra importer : import "@stephane888/wbu-atomique/js/swiper/swiper-drupal.js";
  */
 import SwiperManager from "@stephane888/wbu-atomique/js/swiper/swiper.js";
+const currentProd = {
+  direction: "horizontal",
+  effect: "slide",
+  speed: 5000,
+  spaceBetween: 10,
+  loop: true,
+  zoom: false,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+    enabled: 1,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    enabled: 1,
+  },
+  parallax: true,
+  autoplay: {
+    delay: "1",
+    pauseOnMouseEnter: true,
+    disableOnInteraction: false,
+    waitForTransition: true,
+    stopOnLastSlide: false,
+  },
+  centeredSlides: 0,
+  freeMode: {
+    enabled: true,
+    minimumVelocity: 0.2,
+    momentum: true,
+    momentumBounce: true,
+    momentumBounceRatio: 1,
+    momentumRatio: 1,
+    momentumVelocityRatio: 1,
+    sticky: 1,
+  },
+  slidesPerView: "4",
+  grabCursor: 0,
+};
 /**
  * TOP
  */
