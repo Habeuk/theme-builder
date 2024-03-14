f5da4b5e26a85fb2ba6095e676fd4c57b71dfa3a
vystii <ngahabanda@gmail.com>
2024-03-14 14:09:56 +0100

    gallery overlay avec pagination improvisée de drupal

---
+++ src/gallery-overlay/gallery-overlay.js
@@ -0,0 +1,5 @@
+import "./gallery-overlay.scss";
+// import "./evenex-countdown/countdown"
+import "@stephane888/wbu-atomique/js/bootstrap/all.js";
+
+import "./gallery/gallery";
---
+++ src/gallery-overlay/gallery/gallery.js
@@ -0,0 +1,3 @@
+import "./gallery.html";
+import "./gallery.scss";
+import '@stephane888/wbu-atomique/js/gallery-overlay/tb-gallery';
