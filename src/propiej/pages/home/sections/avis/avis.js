98d139d874d1fbed8232ce47ad2c7563fb5c07ed
kouwa stephane <kksasteph888@gmail.com>
2026-01-16 01:30:50 +0100

    ajout du slider sur avis

--- src/propiej/pages/home/sections/avis/avis.js
+++ src/propiej/pages/home/sections/avis/avis.js
@@ -1,2 +1,12 @@
 import "./avis.html";
 import "./avis.scss";
+// import "@stephane888/wbu-atomique/js/swiper/swiper-drupal.js";
+import SwiperManager from "@stephane888/wbu-atomique/js/swiper/swiper.js";
+
+const AvisSwiper = function (context) {
+  if (document.querySelector(".swiper-full-options", context)) {
+    const sw = new SwiperManager();
+    sw.build();
+  }
+};
+AvisSwiper();
