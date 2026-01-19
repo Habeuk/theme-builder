092c5a9dce4f443e16b5b13cb97624acbdf36be0
kouwa stephane <kksasteph888@gmail.com>
2026-01-19 17:44:28 +0100

    add section video inverse

--- src/propiej/pages/about/about.js
+++ src/propiej/pages/about/about.js
@@ -3,4 +3,5 @@ import "./hero/hero.js";
 import "./partenaires/partenaires.js";
 import "./text_img/text_img.js";
 import "./statistiques/statistiques.js";
+import "./text_video/text_video.js";
 // import "../layouts/footer/footer.js";
---
+++ src/propiej/pages/about/text_video/text_video.js
@@ -0,0 +1,3 @@
+import "./text_video.html";
+import "./text_video.scss";
+import "../../../components/videos_players/videos_players.js";
