62c11ec10fdb7cd74f93fd1d736810e95ac80df6
kouwa stephane <kksasteph888@gmail.com>
2026-02-01 12:27:38 +0100

    Maj

--- src/propiej/propiej.js
+++ src/propiej/propiej.js
@@ -1,8 +1,8 @@
 import "./propiej.scss";
 import "@stephane888/wbu-atomique/js/bootstrap/all.js";
 // home page
-// import "./pages/home/home.js";
+import "./pages/home/home.js";
 // about page
-// import "./pages/about/about.js";
+import "./pages/about/about.js";
 // contact page
 import "./pages/contact/contact.js";
